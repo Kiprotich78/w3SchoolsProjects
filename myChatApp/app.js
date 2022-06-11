759faa8a8ca4ae9e2b3c24f6f79e3144d66e9f11
Kiprotich <shadrackr057@gmail.com>
2022-06-11 17:13:33 +0300

    mychatapp

---
+++ myChatApp/app.js
@@ -0,0 +1,36 @@
+const toggleTab = document.querySelectorAll('.tab'),
+    signUp = document.querySelector('.signup'),
+    login = document.querySelector('.login'),
+    input = document.querySelectorAll('input'),
+    label  = document.querySelectorAll('label');
+
+toggleTab.forEach(tab => { 
+    tab.addEventListener('click', () => {
+        if (!tab.classList.contains('active')) {
+            toggleTab.forEach(tab => {
+                tab.classList.remove('active');
+            });
+            tab.classList.add('active');
+            if (login.classList.contains('appActive')) {
+                login.classList.remove('appActive');
+                signUp.classList.add('appActive');
+            }
+            else {
+                signUp.classList.remove('appActive');
+                login.classList.add('appActive');
+            }      
+        }
+
+    })
+})
+
+input.forEach((input, index) => {
+    input.addEventListener('keyup', () => {
+        if (input.value.length > 0) {
+            label[index].classList.add('labelActive');
+        }
+        else {
+            label[index].classList.remove('labelActive');
+        }
+   })
+});
